11d5666840030134e99629ccfe2a48b6667e8400
Alfred Chan <alfredwchan@gmail.com>
2011-07-28 01:15:32 -0400

    Add option to update vimrc whenever puppet runs.

--- manifests/definitions/vimrc.pp
+++ manifests/definitions/vimrc.pp
@@ -1,4 +1,4 @@
-define vim::vimrc($user = $name, $repo) {
+define vim::vimrc($user = $name, $repo, $update = true) {
 
   file { "/home/${user}/.vimrc":
     content => 'source ~/.vim/vimrc',
@@ -12,6 +12,15 @@ define vim::vimrc($user = $name, $repo) {
     command => "git clone $repo /home/${user}/.vim",
     creates => "/home/${user}/.vim",
     require => Class['git'],
+    notify => Exec["${user} vimrc update"],
+  }
+
+  # update repo on every puppet run if desired
+  if $update {
+    exec { "${user} vimrc update":
+      command => "cd /home/${user}/.vim && git pull && git submodule init && git submodule update",
+      require => Exec["${user} vimrc"],
+    }
   }
 
 }
